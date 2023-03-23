4c62583dae55d09c4da84bde893a7a662f41192e
yunshin <50646145+TransparentDeveloper@users.noreply.github.com>
2023-03-23 21:48:42 +0900

    [Fix] 입력 타입이 int가 아닐 경우 분기 처리

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ maximum = 16; # 사용자 1의 코드를 사용합니다.
 print("Enter N, the length of the horizontal diagonal of the diamond, according to the conditions below.")
 print(" Condition1: ( N must be a odd. )")
 print(" Condition2: ({0} < N < {1})\n".format(0,maximum))
-N = int(input("N : "))
+N = input("N : ")
 print()
 
-if (N <= 0 or N >= maximum):
+if (str(type(N)) != "<class 'int'>"):
+    print("You did not enter an integer.")
+elif (N <= 0 or N >= maximum):
     print("Condition 2 was violated.")
 elif N % 2 == 0:
     print("Condition 1 was violated.")
