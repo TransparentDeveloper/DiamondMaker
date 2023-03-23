4ffe63ad45efebf2570f0e9d0174565c5c1a1816
yunshin <50646145+TransparentDeveloper@users.noreply.github.com>
2023-03-23 22:59:16 +0900

    [Fix] 입력 타입 에러 처리 수정

--- main.py
+++ main.py
@@ -5,22 +5,24 @@ maximum = 16; # 사용자 1의 코드를 사용합니다.
 print("Enter N, the length of the horizontal diagonal of the diamond, according to the conditions below.")
 print(" Condition1: ( N must be a odd. )")
 print(" Condition2: ({0} < N < {1})\n".format(0,maximum))
-N = input("N : ")
-print()
-
-if (str(type(N)) != "<class 'int'>"):
-    print("You did not enter an integer.")
-elif (N <= 0 or N >= maximum):
-    print("Condition 2 was violated.")
-elif N % 2 == 0:
-    print("Condition 1 was violated.")
-else:
-    print("Generates diamonds..")
+try:
+    N = int(input("N : "))
     print()
-    for i in range(N):
-        for j in range(N):
-            if i + j == N // 2 or i - j == N // 2 or j - i == N // 2 or i + j == N // 2 * 3:
-                print("*", end="")
-            else:
-                print(" ", end="")
+    
+    if (N <= 0 or N >= maximum):
+        print("Condition 2 was violated.")
+    elif N % 2 == 0:
+        print("Condition 1 was violated.")
+    else:
+        print("Generates diamonds..")
         print()
+        for i in range(N):
+            for j in range(N):
+                if i + j == N // 2 or i - j == N // 2 or j - i == N // 2 or i + j == N // 2 * 3:
+                    print("*", end="")
+                else:
+                    print(" ", end="")
+            print()
+
+except :
+    print("You did not enter an integer.")
